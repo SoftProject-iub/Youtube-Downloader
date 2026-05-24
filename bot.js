b1419f598af185525bb2986c9aebb51d6b046dd6
SoftProject-iub <s23bseen1m01116@iub.edu.pk>
2026-05-24 07:15:07 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const { Client, LocalAuth, MessageMedia } = pkg;
 ───────────────────────────────────────────── */
 
 const CONFIG = {
-    phoneNumber: "923376201542",
+    phoneNumber: "923376102787",
     clientId: "sheezzi-bot",
     sessionPath: "./session",
     tempPath: "./tmp",
